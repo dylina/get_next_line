07bc63903904b52fc7f6bada26abda01512f1f7c
dylina <109595463+dylina@users.noreply.github.com>
2025-04-28 21:02:59 +0200

    Add files via upload

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dgorceac <dgorceac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 13:22:25 by dgorceac          #+#    #+#             */
-/*   Updated: 2025/04/24 13:38:17 by dgorceac         ###   ########.fr       */
+/*   Updated: 2025/04/28 21:02:40 by dgorceac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,19 @@
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+#include <fcntl.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 5
+
+#endif
 
 char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *source);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
