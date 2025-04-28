c414d1a38107a11c16eceb605e97132741a1c677
dylina <109595463+dylina@users.noreply.github.com>
2025-04-28 21:02:49 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dgorceac <dgorceac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 13:20:30 by dgorceac          #+#    #+#             */
-/*   Updated: 2025/04/24 13:38:16 by dgorceac         ###   ########.fr       */
+/*   Updated: 2025/04/28 21:01:43 by dgorceac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 char	*get_next_line(int fd)
 {
+	static char	*rest;
+	char	*line;
+	char	*buffer;
+	int		bytes;
 
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = (char*)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	while (!ft_strchr(rest, '\n'))
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		rest = ft_strjoin(rest, buffer);
+	}
+	free (buffer);
+	return (line);
 }
+
