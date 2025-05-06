6a7213c2b4162235d9f6d26b3471efed80233a0d
dylina <109595463+dylina@users.noreply.github.com>
2025-05-06 16:59:50 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -2,38 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "get_next_line.h"
-#include <fcntl.h>      // pentru open()
-#include <unistd.h>     // pentru close()
-#include <stdio.h>      // pentru printf()
-#include <stdlib.h>     // pentru free()
 
-char	*get_next_line(int fd); // asigură-te că ai prototipul
+// Main is not included in the project
 
-int	main(int argc, char **argv)
+int	main(void)
 {
 	int		fd;
 	char	*line;
 
-	if (argc != 2)
-	{
-		printf("Usage: %s <filename>\n", argv[0]);
-		return (1);
-	}
-
-	fd = open(argv[1], O_RDONLY);
+	fd = open("test.txt", O_RDONLY);
 	if (fd < 0)
 	{
-		perror("Eroare la deschiderea fișierului");
+		perror("Error");
 		return (1);
 	}
-
-	while ((line = get_next_line(fd)) != NULL)
+	while ((line = get_next_line(fd)))
 	{
-		printf("%s", line); // linia include deja '\n' dacă există
+		printf("line: %s", line);
 		free(line);
 	}
-
 	close(fd);
 	return (0);
 }
-
