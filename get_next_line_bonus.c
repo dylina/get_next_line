7c3957a3129d582111693a57471383c4846aa34b
dylina <109595463+dylina@users.noreply.github.com>
2025-05-06 16:48:51 +0200

    Add files via upload

---
+++ get_next_line_bonus.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgorceac <dgorceac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 15:09:18 by dgorceac          #+#    #+#             */
+/*   Updated: 2025/05/06 15:39:39 by dgorceac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*sub_line(char *rest)
+{
+	int		i;
+	int		j;
+	char	*rest_line;
+
+	i = 0;
+	j = 0;
+	while (rest[i] != '\n' && rest[i] != '\0')
+		i++;
+	if (!rest[i])
+	{
+		free(rest);
+		return (NULL);
+	}
+	rest_line = malloc(sizeof(char) * (ft_strlen(rest) - i + 1));
+	if (!rest_line)
+		return (NULL);
+	i++;
+	while (rest[i])
+		rest_line[j++] = rest[i++];
+	rest_line[j] = '\0';
+	free(rest);
+	return (rest_line);
+}
+
+char	*find_line(char *rest)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (rest[0] == '\0')
+		return (NULL);
+	while (rest[i] && rest[i] != '\n')
+		i++;
+	line = malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (rest[i] && rest[i] != '\n')
+	{
+		line[i] = rest[i];
+		i++;
+	}
+	if (rest[i] == '\n')
+	{
+		line[i] = rest[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+char	*gnl_newfile(int fd, char *save)
+{
+	char	*buffer;
+	int		reader;
+
+	buffer = (char *)malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	reader = 1;
+	while (!ft_strchr(save, '\n') && reader != 0)
+	{
+		reader = read(fd, buffer, BUFFER_SIZE);
+		if (reader <= -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[reader] = '\0';
+		save = ft_strjoin(save, buffer);
+	}
+	free(buffer);
+	return (save);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*save[1024];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	save[fd] = gnl_newfile(fd, save[fd]);
+	if (!save[fd])
+		return (NULL);
+	line = find_line(save[fd]);
+	save[fd] = sub_line(save[fd]);
+	return (line);
+}
